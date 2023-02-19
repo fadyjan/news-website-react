372fda4d5d7388ee2e379866c9747ef8434c7108
fadyjan <fady.jan95@gmail.com>
2023-02-19 17:04:28 +0200

    Create #4 SideBar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar/NavBar';
+import MainContainer from './components/MainContainer/MainContainer';
+import Products from './components/Products/Products';
+import SideBar from './components/SideBar/SideBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="outer-container">
+      {/* <SideBar isOpen={true} pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } right></SideBar> */}
+      <NavBar></NavBar>
+
+      <MainContainer></MainContainer>
+      <Products></Products>
+      
     </div>
   );
 }
